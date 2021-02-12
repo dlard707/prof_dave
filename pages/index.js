16a2971fd60df09a96e503fe3065a7c3d8df9d5a
DAVID ARIEL LARRONDA <dlard707@yahoo.com.ar>
2021-02-12 00:38:01 -0300

    primeira pagina

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 function Home(){
     return <div>
-        
+
         <h1>Página do Profe David</h1>
+        <image src = "https://construct-static.com/uploads/37926/74154a98-1cf5-4b85-96c9-e84a4e9435c9/c/1298669165/yo.jpg">
         
     </div>
 }
